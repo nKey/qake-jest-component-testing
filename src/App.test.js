6d311978ef6360b7d01fbd084beab7440ca98578
Douglas Adriano Queiroz <douglasqueiroz@Mac-mini-de-Douglas.local>
2022-10-26 12:40:50 -0300

    feat new tests

--- src/App.test.js
+++ src/App.test.js
@@ -12,4 +12,6 @@ test("renders learn react link", () => {
   expect(attributeSRC).toEqual(
     "https://br.tinderpressroom.com/image/flame-gradient-RGB_tn1100-category.png"
   );
+  const attributeAlt = linkElementImg.getAttribute("alt");
+  expect(attributeAlt).toEqual("Logo tinder");
 });
